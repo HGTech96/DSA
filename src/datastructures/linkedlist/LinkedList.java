9c006ed3e6346e32df98aa166be042028f51b95b
HGTech96 <gumroyan96@gmail.com>
2023-04-18 16:25:33 +0400

    upd

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -165,9 +165,8 @@ public class LinkedList {
         Node temp = head;
         head = tail;
         tail = temp;
+        Node after = temp.next;
         Node before = null;
-        Node after;
-
         for (int i = 0; i < length; i++) {
             after = temp.next;
             temp.next = before;
